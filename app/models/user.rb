e11b2f134ca1b0254722e76a9145da264d8a7fdc
Ilya Timofeev <il.tmfv@gmail.com>
2016-02-13 23:42:18 +0300

    add tests for user model, add locale for user model attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
     has_many :projects, through: :assignments
     has_many :assignments
     has_many :table_entries
+
+    validates :first_name, :presence => {:message => 'не может быть пустым'}
+    validates :last_name, :presence => {:message => 'не может быть пустой'}
+    validates :position, :presence => {:message => 'не может быть пустой'}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,22 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
     it "create Bob by default" do
-       user = FactoryGirl.create(:user)
+       user = FactoryGirl.build(:user)
        expect(user.first_name).to eq('Bob')
+    end
+
+    it "check if first_name is empty" do
+        user = FactoryGirl.build(:user, :first_name => '')
+        expect(user).to be_invalid
+    end
+
+    it "check if last_name is empty" do
+        user = FactoryGirl.build(:user, :last_name => '')
+        expect(user).to be_invalid
+    end
+
+    it "check if position is empty" do
+        user = FactoryGirl.build(:user, :position => '')
+        expect(user).to be_invalid
     end
 end
