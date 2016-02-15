28f05dacefa47a41e0af9f511a0594929ca68a58
Ilya Timofeev <il.tmfv@gmail.com>
2016-02-15 21:41:40 +0300

    add empty feature test for customer

---
+++ spec/features/edit_customers_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.feature "EditCustomers", type: :feature do
+    pending "add some scenarios (or delete) #{__FILE__}"
+    scenario "User edits customer" do
+
+    end
+end
