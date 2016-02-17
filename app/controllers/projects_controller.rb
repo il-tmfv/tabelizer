55e43b7ef1aff65d564e5b52f38a8ade7483e6a4
Ilya Timofeev <il.tmfv@gmail.com>
2016-02-17 21:41:47 +0300

    add methods, views, tests for prjects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,46 @@
 class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all    
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+      if @project.update_attributes(project_params)
+        redirect_to action: :index
+      else
+        redirect_to action: :edit
+      end
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to action: :index
+    else
+      redirect_to action: :new
+    end
+  end
+
+  def destroy
+   Project.destroy(params[:id])
+   redirect_to action: :index 
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:title, :location, :project, :status)
+  end
+
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
+    it "renders index template" do
+        get :index
+        expect(response).to render_template(:index)
+    end
 
+    it "deletes project" do
+        project = FactoryGirl.create(:project)
+        expect { 
+            delete :destroy, id: project.id
+        }.to change(Project, :count).by(-1)
+    end
+
+    it "creates project" do
+        expect {
+            post :create, project: FactoryGirl.attributes_for(:project)
+        }.to change(Project, :count).by(1)
+    end
+
+    it "creates project and redirects to index" do
+        post :create, project: FactoryGirl.attributes_for(:project)
+        expect(response).to redirect_to(action: :index)
+    end
+
+    it "shows edit project form" do
+        project = FactoryGirl.create(:project)
+        get :edit, id: project.id
+        expect(response).to render_template(:edit)
+    end
+
+    it "redirects to index when updates" do
+        project = FactoryGirl.create(:project)
+        put :update, id: project.id, project: FactoryGirl.attributes_for(:project), comment: 'New comment'
+        expect(response).to redirect_to(action: :index)
+    end
+    
+    it "shows project" do
+        project = FactoryGirl.create(:project)
+        get :show, id: project.id
+        expect(:response).to render_template(:show)
+    end
 end
