cfb6dc7b09a34915b61f15c42ee4d3247300317b
Ilya Timofeev <il.tmfv@gmail.com>
2016-03-02 16:22:57 +0300

    add tests for TableEntries controller

--- spec/controllers/table_entries_controller_spec.rb
+++ spec/controllers/table_entries_controller_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
+require 'rails_helper'
 
 RSpec.describe TableEntriesController, type: :controller do
+  before(:each) do
+    sign_in FactoryGirl.create(:user)
+  end
 
+  it "renders index template" do
+    get :index
+    expect(response).to render_template(:index)
+  end
+
+  it "deletes table entry" do
+    table_entry = FactoryGirl.create(:table_entry)
+    expect { 
+      delete :destroy, id: table_entry.id
+    }.to change(TableEntry, :count).by(-1)
+  end
+
+  it "shows edit table_entry form" do
+    table_entry = FactoryGirl.create(:table_entry)
+    get :edit, id: table_entry.id
+    expect(response).to render_template(:edit)
+  end
+
+  it "redirects to index when updates" do
+    table_entry = FactoryGirl.create(:table_entry)
+    put :update, id: table_entry.id, table_entry: FactoryGirl.attributes_for(:table_entry), comment: 'New comment'
+    expect(response).to redirect_to(action: :index)
+  end
+    
 end
